2e0427be1f9d9b25f84bcc8a7c9c7bd25901a76a
vvihorev <vvihorevv@gmail.com>
2022-11-14 13:45:11 +0100

    Remove solutions >:)

--- problems/even.py
+++
@@ -1,7 +0,0 @@
-def even(n):
-    return n % 2 == 0
-
-
-def test_even():
-    assert even(5) == False
-    assert even(4) == True
--- problems/hello_world.py
+++
@@ -1,6 +0,0 @@
-def greet():
-    return "hello, world!"
-
-
-def test_greet():
-    assert greet() == "hello, world!"
