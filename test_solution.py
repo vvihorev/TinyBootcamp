d58c68809b0eca924107f91b46fe8e9c16a3dd06
vvihorev <vvihorevv@gmail.com>
2022-11-14 13:43:31 +0100

    Add solution tester

--- test_solution.py
+++ test_solution.py
@@ -1,6 +1,12 @@
 import os
+import argparse
 
 
-# os.system("black problems")
-os.system("pytest problems/*")
-os.system("flake8 problems")
+parser = argparse.ArgumentParser()
+parser.add_argument('solution_file')
+args = parser.parse_args()
+solution = args.solution_file
+
+# os.system(f"black problems/{solution}.py")
+os.system(f"pytest problems/{solution}.py")
+os.system(f"flake8 problems/{solution}.py")
