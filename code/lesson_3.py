dc1c9130d6bca1bd692764664c1e164e25a49c12
vvihorev <vvihorevv@gmail.com>
2022-12-05 01:07:05 +0100

    Add class 4

--- code/lesson_3.py
+++ code/lesson_3.py
@@ -1,3 +1,8 @@
+"""
+Примеры решения задач по модулю turtle от МФТИ.
+Можно подсмотреть идеи решений, если попадешь в ступор
+"""
+
 import math
 import time
 import turtle as t
@@ -38,5 +43,19 @@ def draw_circle(r, polygons=30):
     t.right(phi / 2)
 
 
-for r in range(50, 5, -5):
-    draw_circle(r)
+def draw_square(side):
+    for _ in range(4):
+        t.forward(side)
+        t.left(90)
+
+# for r in range(50, 5, -5):
+    # draw_circle(r)
+
+start = 10
+for _ in range(10):
+    t.penup()
+    t.goto(-start, -start)
+    t.pendown()
+    draw_square(start*2)
+    start += 10
+time.sleep(1)
